7491b7947ccc08e2529f38a924a91df86e8bc764
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-05-04 21:59:23 +0630

    update Task

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -27,14 +27,11 @@ class TaskController extends Controller
         return TaskResource::make($task);
     }
 
-    public function edit(Task $task)
-    {
-        //
-    }
-
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+
+        return TaskResource::make($task);
     }
 
     public function destroy(Task $task)
