483443fef2c57f2f722caedbc72e2179b3b00338
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-05-26 23:57:42 +0630

    Update: login func of /auth/login (session base)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,17 +15,15 @@ class LoginController extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        if (!auth()->attempt($request->only(['email', 'password']))) {
             throw ValidationException::withMessages([
                 'email' => ['The credentials you entered are incorrect.']
             ]);
         }
 
         return response()->json([
-            'user' => $user,
-            'token' => $user->createToken('laravel_api_token')->plainTextToken
+            'message' => 'Login successful',
+            'user' => auth()->user(),
         ]);
     }
 }
